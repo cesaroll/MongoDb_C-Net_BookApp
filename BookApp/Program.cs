1db34bfd97da6ab12a093ff1ae280fca2f19caf3
Cesar Lopez <cesarl@outlook.com>
2016-02-15 22:29:36 -0700

    Adding all Books using ImsertMany

--- BookApp/Program.cs
+++ BookApp/Program.cs
@@ -15,46 +15,69 @@ namespace BookApp
             var bookCtx = new BookContext();
             long count = 0;
 
+            var books = new List<Book>();
+
             Console.WriteLine("Loading Books ...");
-            /*
+            
             foreach (var item in GetBookData())
             {
-                var book = new Book()
+                try
                 {
-                    ISBN = item[0],
-                    Title = item[1],
-                    Author = item[2],
-                    PublicationYear = int.Parse(item[3]),
-                    Publisher = item[4],
-                    ImageUrlSmall = item[5],
-                    ImageUrlMedium = item[6],
-                    ImageUrlLarge = item[7],
-                };
+                    var book = new Book()
+                    {
+                        ISBN = item[0],
+                        Title = item[1],
+                        Author = item[2],
+                        PublicationYear = int.Parse(item[3]),
+                        Publisher = item[4],
+                        ImageUrlSmall = item[5],
+                        ImageUrlMedium = item[6],
+                        ImageUrlLarge = item[7],
+                    };
 
-                bookCtx.Books.InsertOne(book);
-                count++;
+                    books.Add(book);
+                    count++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error in line: {0}", count+1);
+                }
 
-                Console.WriteLine("{0,-10} - {1}", count,book.Title);
+                //Insert every 1,000 records
+                if ((count % 1000) == 0)
+                {
+                    bookCtx.Books.InsertMany(books);
+                    Console.WriteLine("Inserting: {0}", count);
+                    books = new List<Book>();
+                }
             }
-            
+
+            //Insert last if any
+            if (books.Count > 0)
+            {
+                bookCtx.Books.InsertMany(books);
+                Console.WriteLine("Inserting: {0}", count);
+                books = null;
+            }
+
             Console.WriteLine("Finished!");
-            */
+            
 
             // Read some
-
+            /*
             var books = Queryable.Where(bookCtx.Books.AsQueryable(), b => b.PageCount > 50);
             Console.WriteLine("Books with more than 50 pages: {0}", books.Count());
             Console.WriteLine("First One: {0}", books.FirstOrDefault().Title);
             Console.ReadKey();
-
+            */
         }
 
         public static IEnumerable<string[]> GetBookData()
         {
             string filename = @"D:\Temp\BX-CSV-Dump\BX-Books.csv";
-            var bookLines = File.ReadLines(filename).Take(100);
+            var bookLines = File.ReadLines(filename);
             return bookLines.Skip(1).Select(s => s.Replace("\"", "").Replace("&amp;", "&").Split(';'));
         }
-
+        
     }
 }
